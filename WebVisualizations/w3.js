baaa50a030e5a45d4765f6b01171aff24110c044
toddhnstl <todd.heritage@att.net>
2019-10-02 14:06:02 -0500

    commit of change to git hub file

--- WebVisualizations/w3.js
+++ WebVisualizations/w3.js
@@ -240,7 +240,7 @@ w3.includeHTML = function(cb) {
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4) {
           if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 0) {elmnt.innerHTML = this.responseText;}
+
           if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
           elmnt.removeAttribute("w3-include-html");
           w3.includeHTML(cb);
