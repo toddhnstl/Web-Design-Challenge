061982a2248844c8f12abeb354e662bafc900a51
toddhnstl <todd.heritage@att.net>
2019-10-03 14:08:41 -0500

    updates/anchor targ = blank

--- Resources/cities_to_html.py
+++ Resources/cities_to_html.py
@@ -1,31 +1,51 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[20]:
-
+# In[1]:
 
 
 import pandas as pd
 import numpy as np
 import os
 
-cities_data_file = "cities.csv"
+cities_data_input = "cities.csv"
 
-cities_data_html_file = os.path.join('..', 'WebVisualizations', 'cities_data_table.html')
-
-
-
-# In[21]:
+cities_data_html_file = os.path.join('../', 'WebVisualizations',  'cities_data_table.html')
 
 
-cityWeatherDF = pd.read_csv(cities_data_file)
+# In[2]:
+
+
+cityWeatherDF = pd.read_csv(cities_data_input)
 cityWeatherDF.head()
 
 
-# In[22]:
+# In[3]:
 
 
-cityWeatherDF.to_html(cities_data_html_file, index=False)
+
+ll_city = []
+for index, row in cityWeatherDF.iterrows():
+    this_city = row['City']    
+    lat = row['Lat']
+    lng = row['Lng']
+    str = f'<a href="https://www.google.com/maps/@{lat},{lng},11z" target="_blank">{this_city}</a>'
+    ll_city.append(str)
+
+cityWeatherDF["City"] = ll_city   
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[4]:
+
+
+cityWeatherDF.to_html(cities_data_html_file, index=False, escape=False)
 
 
 # In[ ]:
